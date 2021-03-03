cad7f8193986d46286490397f26a6b8c3736c09e
Nilakshi-coder <55271036+Nilakshi-coder@users.noreply.github.com>
2021-03-04 00:41:39 +0530

    TestCase_1
    1.1. method takes up empty string as argument and returns 0
    1.2. method takes single number and returns same number itself
    1.3 method takes two numbers (comma seperated) and returns sum of two numbers

--- src/main/java/com/np/tdd/calculator/StringCalculator.java
+++ src/main/java/com/np/tdd/calculator/StringCalculator.java
@@ -3,7 +3,13 @@ package com.np.tdd.calculator;
 public class StringCalculator {
 	
 	public int Add(String numbers) {
-		return 0;
+		if(numbers.isEmpty())
+			return 0;
+		else
+			if(numbers.contains(",")) {
+				return Integer.parseInt(numbers.split(",")[0]) + Integer.parseInt(numbers.split(",")[1]);
+			}
+			return Integer.parseInt(numbers);
 	}
 
 }
--- src/test/java/com/np/tdd/calculator/StringCalculatorTest.java
+++ src/test/java/com/np/tdd/calculator/StringCalculatorTest.java
@@ -1,15 +1,33 @@
 package com.np.tdd.calculator;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTest {
+	
+	private StringCalculator calculator = null;
+	
+	@Before
+	public void init() {
+		calculator = new StringCalculator();
+		System.out.println(calculator.toString());
+	}
 
 	@Test
 	public void emptyString_Returns0() {
-		StringCalculator calculator = new StringCalculator();
 		assertEquals(0, calculator.Add(""));
+	}
+	
+	@Test
+	public void singleNumber_ReturnsSameNumber() {
+		assertEquals(1, calculator.Add("1"));
+	}
+	
+	@Test
+	public void twoNumbers_ReturnsSumOfTwoNumbers() {
+		assertEquals(3, calculator.Add("1,2"));
 	}
 
 }
